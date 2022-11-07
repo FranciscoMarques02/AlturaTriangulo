cc82a207f2cbb08a6435ced6742739308dce9238
FranciscoMarques02 <117680516+FranciscoMarques02@users.noreply.github.com>
2022-11-07 16:48:45 -0300

    Initial commit

---
+++ AlturaTriangulo/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace AlturaTriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.txbBase = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBase.Location = new System.Drawing.Point(23, 120);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(206, 20);
+            this.lblBase.TabIndex = 0;
+            this.lblBase.Text = "BASE DO TRIÂNGULO: ";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAltura.Location = new System.Drawing.Point(23, 194);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(223, 20);
+            this.lblAltura.TabIndex = 1;
+            this.lblAltura.Text = "ALTURA DO TRIÂNGULO:";
+            // 
+            // txbBase
+            // 
+            this.txbBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbBase.Location = new System.Drawing.Point(252, 111);
+            this.txbBase.Name = "txbBase";
+            this.txbBase.Size = new System.Drawing.Size(71, 29);
+            this.txbBase.TabIndex = 2;
+            this.txbBase.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txbAltura.Location = new System.Drawing.Point(252, 185);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(71, 29);
+            this.txbAltura.TabIndex = 3;
+            this.txbAltura.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(21, 311);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(101, 31);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "ÁREA:";
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(29, 33);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(294, 31);
+            this.lblTitulo.TabIndex = 6;
+            this.lblTitulo.Text = "ÁREA CALCULATOR";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCalcular.Location = new System.Drawing.Point(117, 255);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(138, 23);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "CALCULAR";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(359, 396);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbBase);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "ALTURA DO TRIÂNGULO";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.TextBox txbBase;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ AlturaTriangulo/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AlturaTriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            // FÓRMULA: (base * altura) / 2            
+            try
+            {
+                double resultado = double.Parse(txbBase.Text) * double.Parse(txbAltura.Text) / 2;
+                lblResultado.Text = "ÁREA: " + resultado.ToString();
+                lblResultado.ForeColor = Color.Black;
+            }
+            catch
+            {
+                lblResultado.Text = "ERRO!";
+                lblResultado.ForeColor = Color.Red;
+            }
+            
+            
+        }
+    }
+}
